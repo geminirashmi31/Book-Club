cbcaaa36bd8310840fd233c3cce8664d9e596264
Lynn Kuhlman <kuhlman.lynn@gmail.com>
2019-04-02 09:43:59 -0700

    Adding data migration

---
+++ BookAPI/Migrations/20190402015750_rating.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace BookAPI.Migrations
+{
+    public partial class rating : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Rating",
+                table: "Books",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldMaxLength: 100);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Rating",
+                table: "Books",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(int),
+                oldMaxLength: 100);
+        }
+    }
+}
