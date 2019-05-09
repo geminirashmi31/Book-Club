1c67e8f37afd6d641ae934371db5a0e845c80394
Rashmi <geminirashmi31@gmail.com>
2019-05-08 18:34:59 -0700

    Added CartApi

---
+++ CartApi/Model/CartItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CartApi.Model
+{
+    public class CartItem
+    {
+        public string Id { get; set; }
+        public string BookId { get; set; }
+        public string BookName { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal OldUnitPrice { get; set; }
+        public int Quantity { get; set; }
+        public string PictureUrl { get; set; }
+    }
+}
